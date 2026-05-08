e1ff0fe2084ca9c43142812bab277d3a514493c8
Zyganadel <121130526+Zyganadel@users.noreply.github.com>
2026-05-08 13:19:03 -0300

    updated res paths

--- examples/scenes/end_credits/end_credits.gd
+++ examples/scenes/end_credits/end_credits.gd
@@ -1,5 +1,5 @@
 @tool
-extends "res://addons/maaacks_credits_scene/examples/scenes/credits/scrolling_credits.gd"
+extends "res://addons/Godot-Credits-Scene/examples/scenes/credits/scrolling_credits.gd"
 
 ## Defines the path to the main menu. Hides the Main Menu button if not set.
 @export_file("*.tscn") var main_menu_scene_path : String
